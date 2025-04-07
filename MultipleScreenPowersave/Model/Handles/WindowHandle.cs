125668d468c0b042e5512fa84502745bc9f72bf3
moormaster <moormaster@gmx.net>
2025-04-07 06:44:18 +0200

    feat: add WindowHandle

---
+++ MultipleScreenPowersave/Model/Handles/WindowHandle.cs
@@ -0,0 +1,10 @@
+﻿namespace MultipleScreenPowersave.Model.Handles;
+
+using StronglyTypedIds;
+using Windows.Win32;
+
+/// <summary>
+/// Strongly typed handle to a window as set by <see cref="PInvoke.EnumWindows(Windows.Win32.UI.WindowsAndMessaging.WNDENUMPROC, Windows.Win32.Foundation.LPARAM)"/>.
+/// </summary>
+[StronglyTypedId(Template.Int)]
+public readonly partial struct WindowHandle;
