58e9d392a00ef33a18e823e25e5bc6915a66b425
moormaster <moormaster@gmx.net>
2025-04-29 18:44:55 +0200

    feat(DisplayControlServiceFacade): When turning on a monitor try the same displayControlService first it was turned off with before

--- MultipleScreenPowersave/App/DisplayControlServiceFacade.cs
+++ MultipleScreenPowersave/App/DisplayControlServiceFacade.cs
@@ -20,6 +20,19 @@ public class DisplayControlServiceFacade(
     ILogger<DisplayControlServiceFacade> logger
 ) : IDisplayControlServiceFacade
 {
+    private readonly Dictionary<
+        PhysicalMonitorInformation,
+        DisplayControlServiceType
+    > displayControlServiceTypeMonitorWasTurnedOffWith = new Dictionary<
+        PhysicalMonitorInformation,
+        DisplayControlServiceType
+    >(
+        EqualityComparer<PhysicalMonitorInformation>.Create(
+            ComparePhysicalMonitorInformationIdentity,
+            getHashCode: physicalMonitorInformation => 1
+        )
+    );
+
     private Exception? lastException;
 
     /// <inheritdoc/>
@@ -28,6 +41,17 @@ public class DisplayControlServiceFacade(
         DisplayMonitorInformation virtualMonitor
     )
     {
+        if (
+            this.displayControlServiceTypeMonitorWasTurnedOffWith.TryGetValue(
+                physicalMonitor,
+                out _
+            )
+        )
+        {
+            // prevent turning off monitor twice
+            return;
+        }
+
         if (
             !(
                 this.TryTurnOffMonitorUsingDdc(physicalMonitor, virtualMonitor)
@@ -41,14 +65,72 @@ public class DisplayControlServiceFacade(
     /// <inheritdoc/>
     public void TurnOnMonitor(PhysicalMonitorInformation monitor)
     {
+        bool success = false;
+
+        if (
+            this.displayControlServiceTypeMonitorWasTurnedOffWith.TryGetValue(
+                monitor,
+                out var displayControlServiceType
+            )
+        )
+        {
+            // try to turn on monitor using the same service it was successfully turned off with before.
+            success = displayControlServiceType switch
+            {
+                DisplayControlServiceType.Ddc => this.TryTurnOnMonitorUsingDdc(monitor),
+                DisplayControlServiceType.Backlight => this.TryTurnOnMonitorUsingBacklight(monitor),
+                DisplayControlServiceType.BlackWindow => this.TryTurnOnMonitorUsingBlackWindow(
+                    monitor
+                ),
+                _ => false,
+            };
+        }
+
+        // if service is unknown try every service until success
         if (
             !(
-                this.TryTurnOnMonitorUsingDdc(monitor)
+                success
+                || this.TryTurnOnMonitorUsingDdc(monitor)
                 || this.TryTurnOnMonitorUsingBacklight(monitor)
                 || this.TryTurnOnMonitorUsingBlackWindow(monitor)
             )
         )
             throw this.lastException!;
+    }
+
+    private static bool ComparePhysicalMonitorInformationIdentity(
+        PhysicalMonitorInformation? physicalMonitorInformation1,
+        PhysicalMonitorInformation? physicalMonitorInformation2
+    )
+    {
+        if ((physicalMonitorInformation1 == null) ^ (physicalMonitorInformation2 == null))
+            return false;
+
+        if (physicalMonitorInformation1 == null && physicalMonitorInformation1 == null)
+            return true;
+
+        if (physicalMonitorInformation1.Handle == physicalMonitorInformation2?.Handle)
+            return true;
+
+        if (!string.IsNullOrEmpty(physicalMonitorInformation1.EdidHex))
+        {
+            if (physicalMonitorInformation1.EdidHex == physicalMonitorInformation2?.EdidHex)
+                return true;
+        }
+
+        if (!string.IsNullOrEmpty(physicalMonitorInformation1.WmiInstanceName))
+        {
+            if (
+                physicalMonitorInformation1.WmiInstanceName
+                == physicalMonitorInformation2?.WmiInstanceName
+            )
+                return true;
+        }
+
+        if (physicalMonitorInformation1.Index == physicalMonitorInformation2?.Index)
+            return true;
+
+        return false;
     }
 
     private bool TryTurnOnMonitorUsingDdc(PhysicalMonitorInformation monitor)
@@ -57,6 +139,7 @@ public class DisplayControlServiceFacade(
         {
             displayDataChannelService.TurnOnMonitor(monitor);
 
+            this.displayControlServiceTypeMonitorWasTurnedOffWith.Remove(monitor);
             return true;
         }
         catch (Exception e)
@@ -83,6 +166,7 @@ public class DisplayControlServiceFacade(
             );
             displayBacklightService.TurnOnMonitor(monitor);
 
+            this.displayControlServiceTypeMonitorWasTurnedOffWith.Remove(monitor);
             return true;
         }
         catch (Exception e)
@@ -109,6 +193,7 @@ public class DisplayControlServiceFacade(
             );
             blackWindowService.TurnOnMonitor(monitor);
 
+            this.displayControlServiceTypeMonitorWasTurnedOffWith.Remove(monitor);
             return true;
         }
         catch (Exception e)
@@ -134,6 +219,8 @@ public class DisplayControlServiceFacade(
         {
             displayDataChannelService.TurnOffMonitor(physicalMonitor, virtualMonitor);
 
+            this.displayControlServiceTypeMonitorWasTurnedOffWith[physicalMonitor] =
+                DisplayControlServiceType.Ddc;
             return true;
         }
         catch (Exception e)
@@ -163,6 +250,8 @@ public class DisplayControlServiceFacade(
             );
             displayBacklightService.TurnOffMonitor(physicalMonitor, virtualMonitor);
 
+            this.displayControlServiceTypeMonitorWasTurnedOffWith[physicalMonitor] =
+                DisplayControlServiceType.Backlight;
             return true;
         }
         catch (Exception e)
@@ -192,6 +281,8 @@ public class DisplayControlServiceFacade(
             );
             blackWindowService.TurnOffMonitor(physicalMonitor, virtualMonitor);
 
+            this.displayControlServiceTypeMonitorWasTurnedOffWith[physicalMonitor] =
+                DisplayControlServiceType.BlackWindow;
             return true;
         }
         catch (Exception e)
---
+++ MultipleScreenPowersave/App/DisplayControlServiceType.cs
@@ -0,0 +1,22 @@
+﻿namespace MultipleScreenPowersave.App;
+
+/// <summary>
+/// Enum describing the type of <see cref="IDisplayControlService"/>.
+/// </summary>
+public enum DisplayControlServiceType
+{
+    /// <summary>
+    /// DisplayControl using DDC.
+    /// </summary>
+    Ddc,
+
+    /// <summary>
+    /// DisplayControl using backlight.
+    /// </summary>
+    Backlight,
+
+    /// <summary>
+    /// DisplayControl using black window.
+    /// </summary>
+    BlackWindow,
+}
