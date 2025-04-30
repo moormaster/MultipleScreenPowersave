31b5ef5f30b732eaee89556d731e0f25b4a3b8b3
moormaster <moormaster@gmx.net>
2025-04-30 20:00:26 +0200

    feat!: remove linux prototype to query monitors

--- prototypes/MonitorInfoServer/DataModels/VirtualMonitor.cs
+++
@@ -1,9 +0,0 @@
-namespace DataModels;
-
-public record VirtualMonitor
-{
-    public string OutputName { get; set; }
-    public string Resolution { get; set; }
-    public string Position { get; set; }
-    public string? EDID { get; set; }
-}
